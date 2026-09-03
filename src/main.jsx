41d5fe874deb47072f74fb57bbbbbd7544568a83
Fico218 <jrss317031@gmail.com>
2026-09-03 03:24:44 -0500

    Fix React error boundary import

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { Component, StrictMode } from "react";
+import React, { Component, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
